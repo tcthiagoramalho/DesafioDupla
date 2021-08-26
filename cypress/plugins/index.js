582943efd56749e493f6f3927f40c7bdc54283e9
tcthiagoramalho <thiago.barbosa@tc.com.br>
2021-08-26 17:37:03 -0300

    ArquiteturaPt3

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -12,11 +12,15 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
+
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+}
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
+module.exports = (on,config) => {
+  on('file:preprocessor' , cucumber())
 }
