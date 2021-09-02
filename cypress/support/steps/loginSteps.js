67f59fad5cbee5fde1393825dbfec35d61e1b2ac
tcthiagoramalho <thiago.barbosa@tc.com.br>
2021-09-02 14:52:08 -0300

    melhoria nos steps

--- cypress/support/steps/loginSteps.js
+++ cypress/support/steps/loginSteps.js
@@ -1,16 +1,10 @@
 /* global Given, And, When, Then*/
 
-import acessarSite from "../pageobjects/acessarSite"
 import users from "../pageobjects/usersPage"
 import loginPage from "../pageobjects/loginPage"
 
-const urlSite = new acessarSite
 const login = new users
 const logar = new loginPage 
-
-Given(/^que acesso o site$/, () => {
-	urlSite.AbrirUrl();
-});
 
 When(/^informo os dados incorretos$/, () => {
 	logar.preencherDadosDeLogin('thiago', '12345');
